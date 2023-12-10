eb33975a7f5fb0572451471f03c3a614b97848dd
Danrlei <danrleidalfre@gmail.com>
2023-12-10 11:21:40 -0300

    fix css and labels

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -3,8 +3,8 @@ import styles from "./Info.module.css";
 export function Info() {
     return (
         <section className={styles.info}>
-            <h2>Tarefas criadas <span>0</span></h2>
-            <h2>Tarefas concluídas <span>0</span></h2>
+            <h2>Criadas <span>0</span></h2>
+            <h2>Concluídas <span>0</span></h2>
         </section>
     )
 }
